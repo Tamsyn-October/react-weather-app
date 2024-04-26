72cd2c05739c9970833b33bde322f516982c4667
Tamsyn October <tamsynoctober15@gmail.com>
2024-04-26 14:39:00 +0200

    Added github repo link

--- src/App.js
+++ src/App.js
@@ -2,5 +2,16 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather Application</div>;
+  return (
+    <div className="App">
+      {" "}
+      <h1>Weather Application</h1>
+      <footer>
+        This project was coded by Tamsyn October and is{" "}
+        <a href="" target="_blank">
+          open sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
